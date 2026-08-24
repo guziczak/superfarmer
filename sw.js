282a6ba5811574638c09bb2a3c7d0f4e6eefd55a
guziczak <guziczak@proton.me>
2026-08-24 21:04:35 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Superfarmer 3D — cache offline (network-first: online zawsze świeża wersja).
-const VER = 'sf3d-af47d5b316';
+const VER = 'sf3d-ed977a7f16';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(VER).then((c) => c.addAll(['./'])).then(() => self.skipWaiting()));
 });
